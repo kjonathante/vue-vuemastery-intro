ed7d61a3bc75b23b391daa6a3e40bb5c21905c6b
Kit Jonathan Te <kjonathante@gmail.com>
2019-06-16 14:15:13 -0700

    Created a new component. Imported it to the App component.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
     {{ message }}
+    <Product />
   </div>
 </template>
 
 
 <script>
+import Product from "./Product.vue"
+
 export default {
   data() {
     return {
-      message: 'Hello World',
+      message: "Hello World"
     };
   },
+  components: { Product }
 };
 </script>
 
 <style>
 #app {
   font-size: 18px;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   color: blue;
 }
 </style>
---
+++ src/Product.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{product}}</h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      product: "socks"
+    };
+  }
+};
+</script>
